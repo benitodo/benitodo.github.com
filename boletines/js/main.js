c1c50a06eb7967517a3a20bd36cc674daea60bbb
benitodo <benitdiaz@gmail.com>
2022-06-11 23:12:43 -0500

    Agregamos proyecto boletines

---
+++ boletines/js/main.js
@@ -0,0 +1,34 @@
+console.log("Hola mundo desde Java script");
+
+//1. DOM se pone el objeto document que se refiere a todo el documento html
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(links)
+{
+    console.log(links);
+}
+);
+*/
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link)
+{
+    link.addEventListener("click",function(evento){
+        evento.preventDefault();
+
+        return false;
+
+    })
+});
+
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-regular");
+    icono.classList.add("fa-regular");
+    
+    
+})
+
