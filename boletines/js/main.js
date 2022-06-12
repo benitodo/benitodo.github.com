0ab44861acd74e2b4267f5d69904ba4d250a5c1d
benitodo <benitdiaz@gmail.com>
2022-06-11 23:22:25 -0500

    Update main.js
    quité la prevención del evento de click

--- boletines/js/main.js
+++ boletines/js/main.js
@@ -16,9 +16,9 @@ let links = document.querySelectorAll(".close");
 links.forEach(function(link)
 {
     link.addEventListener("click",function(evento){
-        evento.preventDefault();
+       
 
-        return false;
+        
 
     })
 });
